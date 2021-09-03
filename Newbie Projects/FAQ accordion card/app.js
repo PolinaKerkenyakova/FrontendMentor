43429eac30e6df26b7b0a94976f7e529dfdde7c1
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-09-03 15:47:56 +0300

    Mobile-first design code added

--- Newbie Projects/FAQ accordion card/app.js
+++ Newbie Projects/FAQ accordion card/app.js
@@ -2,18 +2,20 @@ document.querySelectorAll('.question__details').forEach(question => {
     question.addEventListener('click', showHideAnswer);
 });
 
-const arrow = document.querySelector('.question__arrow');
-
 function showHideAnswer(event) {
 
-    const question = event.target.closest('.question')
+    const question = event.target.closest('.question');
+    const questionText = question.querySelector('.question__text')
     const answer = question.querySelector('.qustion__answear');
+    const arrow = question.querySelector('.question__arrow');
 
     if (answer.classList.contains('hidden')) {
         answer.classList.remove('hidden');
         arrow.classList.add('rotate-arrow');
+        questionText.classList.add('highlight-question');
     } else {
         answer.classList.add('hidden');
         arrow.classList.remove('rotate-arrow');
+        questionText.classList.remove('highlight-question');
     }
 }
